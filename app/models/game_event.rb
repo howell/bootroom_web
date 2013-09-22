495750046492d6af9a338ac7b50e3554c8d2863b
Sam Caldwell <samcaldwell@utexas.edu>
2013-09-21 22:36:33 -0500

    add basic report

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -3,6 +3,7 @@ class GameEvent < ActiveRecord::Base
   belongs_to :player
   validates :player_id, presence: true
 
+
   def event_names
     { 1 => "Pass",
       2 => "Shot",
@@ -40,5 +41,75 @@ class GameEvent < ActiveRecord::Base
 
   def has_other_player
     !(other_player_id.blank? || other_player_id == 0)
+  end
+
+  def self.match_report(game_events)
+    match_report = { }
+    match_report[:passing] = passing_report(game_events)
+    match_report[:shooting] = shooting_report(game_events)
+    match_report[:defending] = defending_report(game_events)
+    match_report
+  end
+
+  def self.passing_report(game_events)
+    passing_report = { total: 0, complete: 0, incomplete: 0,
+                       key: 0, assists: 0 }
+    passing_events = game_events.select { |ge| ge.event_type == 1 }
+    passing_events.each do |pass|
+      passing_report[:total] += 1
+      case pass.event_subtype
+      when 1  # Complete
+        passing_report[:complete] += 1
+      when 2 # Incomplete
+        passing_report[:incomplete] += 1
+      when 3 # Key
+        passing_report[:key] += 1
+        passing_report[:complete] += 1
+      when 4 # Assist
+        passing_report[:key] += 1
+        passing_report[:complete] += 1
+        passing_report[:assists] += 1
+      else
+      end
+    end
+    if passing_report[:total] == 0
+      passing_report[:accuracy] = 0
+    else
+      passing_report[:accuracy] = (passing_report[:complete] * 100) /
+        passing_report[:total]
+    end
+    passing_report
+  end
+
+  def self.shooting_report(game_events)
+    shooting_report = { total: 0, on_target: 0, off_target: 0, goals: 0 }
+    shooting_events = game_events.select { |ge| ge.event_type == 2 }
+    shooting_events.each do |shot|
+      shooting_report[:total] += 1
+      case shot.event_subtype
+      when 1 # On Target
+        shooting_report[:on_target] += 1
+      when 2 # Off Target
+        shooting_report[:off_target] += 1
+      when 3 # Goal
+        shooting_report[:on_target] += 1
+        shooting_report[:goals] += 1
+      else
+      end
+    end
+    if shooting_report[:total] == 0
+      shooting_report[:accuracy] = 0
+    else
+      shooting_report[:accuracy] = (shooting_report[:on_target] * 100) /
+        shooting_report[:total]
+    end
+    shooting_report
+  end
+
+  def self.defending_report(game_events)
+    defending_report = { }
+    defending_events = game_events.select { |ge| ge.event_type == 5 }
+    defending_report[:tackles] = defending_events.count
+    defending_report
   end
 end
