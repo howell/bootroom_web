47131495667f4045c2707dceed3a5b4acfb42626
Sam Caldwell <samcaldwell@utexas.edu>
2013-09-21 19:51:45 -0500

    update event names to use a hash

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -3,95 +3,38 @@ class GameEvent < ActiveRecord::Base
   belongs_to :player
   validates :player_id, presence: true
 
+  def event_names
+    { 1 => "Pass",
+      2 => "Shot",
+      3 => "Substitution",
+      4 => "Shot against",
+      5 => "Tackle",
+      6 => "Foul",
+      7 => "Yellow Card",
+      8 => "Red Card" }
+  end
+
+  def event_subtype_names
+    { 1 => { 1 => "Complete", 2 => "Incomplete", 3 => "Key",
+             4 => "Assist"},
+      2 => { 1 => "On Target", 2 => "Off Target", 3 => "Goal" },
+      3 => { 1 => "Sub On", 2 => "Sub Off" },
+      4 => { 1 => "Save", 2 => "Conceded" } }
+  end
+
   def game_time
     "#{ (timestamp / 60) + 1 }'"
   end
 
   def event_name
-    case event_type
-    when 1
-      "Pass"
-    when 2
-      "Shot"
-    when 3
-      "Substitution"
-    when 4
-      "Shot against"
-    when 5
-      "Tackle"
-    when 6
-      "Foul"
-    when 7
-      "Yellow Card"
-    when 8
-      "Red Card"
-    else
-      "Undefined Event"
-    end
+    event_names[event_type]
   end
 
   def subtype_name
-    case event_type
-    when 1
-      pass_subtype
-    when 2
-      shot_subtype
-    when 3
-      substitution_subtype
-    when 4
-      shot_against_subtype
+    if event_subtype_names[event_type].nil?
+      nil
     else
-      "Undefined"
-    end
-  end
-
-  def pass_subtype
-    case event_subtype
-    when 1
-      "Complete"
-    when 2
-      "Incomplete"
-    when 3
-      "Key"
-    when 4
-      "Assist"
-    else
-      "Undefined"
-    end
-  end
-
-  def shot_subtype
-    case event_subtype
-    when 1
-      "On Target"
-    when 2
-      "Off Target"
-    when 3
-      "Goal"
-    else
-      "Undefined"
-    end
-  end
-
-  def substitution_subtype
-    case event_subtype
-    when 1
-      "Sub On"
-    when 2
-      "Sub Off"
-    else
-      "Undefined"
-    end
-  end
-
-  def shot_against_subtype
-    case event_subtype
-    when 1
-      "Save"
-    when 2
-      "Conceded"
-    else
-      "Undefined"
+      event_subtype_names[event_type][event_subtype]
     end
   end
 
